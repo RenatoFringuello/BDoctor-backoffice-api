f99f67fb5692078ccbdee485a96ea00740a5c0e0
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-03-16 16:08:45 +0100

    Creating Users Table with Migration, Seeder, Model

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $newUser = new User();
+        $newUser->name = 'Gino';
+        $newUser->lastname = 'Ginetti';
+        $newUser->email = 'g@gmail.com';
+        $newUser->password = Hash::make(12345678);
+        $newUser->isActive = true;
+        $newUser->save();
+    }
+}
