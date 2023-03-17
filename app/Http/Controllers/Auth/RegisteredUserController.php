e646aebee8d301ad6a1cdb7090326061a570b68e
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-03-17 18:22:32 +0100

    create store crud for profile table

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\Specialization;
 use App\Models\User;
+use App\Models\Profile;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -32,28 +33,50 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        $request->validate([
+        $data = $request->validate([
+            // Required
             'name' => ['required', 'string', 'max:255'],
             'lastname' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
             'address' => ['required', 'string', 'max:255'],
-            'specializations' => "required|array|min:1|exists:specializations,id",
-
+            'specializations' => ['required', 'array', 'min:1', 'exists:specializations,id'],
+            // Not Required
+            'picture' => ['image', 'nullable'],
+            'bio' => ['string'],
+            'services' => ['string'],
+            'telephone' => ['string', 'max:13', 'regex:/^[0-9]+$/'],
+            'curriculum' => ['image', 'nullable'],
         ]);
 
         // dd($request);
 
-        $user = User::create([
-            'name' => $request->name,
-            'lastname' => $request->lastname,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        // $user = User::create([
+        //     'name' => $request->name,
+        //     'lastname' => $request->lastname,
+        //     'email' => $request->email,
+        //     'password' => Hash::make($request->password),
+        // ]);
+
+        // User Data
+        $user = new User();
+        $user->fill($data);
+        $user->password = Hash::make($request->password);
+        $user->isActive = true;
+        $user->save();
+
+        // dd(User::where('email', $user->email)->pluck('id'));
+
+        // Profile Data
+        $profile = new Profile();
+        $profile->fill($data);
+        $profile->user_id = User::where('email', $user->email)->pluck('id')[0];
+        $profile->save();
 
         event(new Registered($user));
 
         Auth::login($user);
+
 
         return redirect(RouteServiceProvider::HOME);
     }
