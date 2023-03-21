7a725ee490f7cfc57606e23e2792a290005edd9e
Shelbot77 <alessiodeangelis021@gmail.com>
2023-03-21 11:06:27 +0100

    setup api for all users

--- app/Http/Controllers/api/ApiDoctorsController.php
+++ app/Http/Controllers/api/ApiDoctorsController.php
@@ -4,8 +4,21 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Profile;
+use App\Models\Specialization;
+use App\Models\Sponsor;
 
 class ApiDoctorsController extends Controller
 {
     //
+
+    public function index()
+    {
+        $users = User::all();
+        return response()->json([
+            'success' => true,
+            'results' => $users
+        ]);
+    }
 }
