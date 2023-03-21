2bb76da6820ac7ed301b711153742833b004f6c6
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-03-21 11:27:46 +0100

    setup api with relationship profile

--- app/Http/Controllers/api/ApiDoctorsController.php
+++ app/Http/Controllers/api/ApiDoctorsController.php
@@ -5,9 +5,9 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-use App\Models\Profile;
-use App\Models\Specialization;
-use App\Models\Sponsor;
+// use App\Models\Profile;
+// use App\Models\Specialization;
+// use App\Models\Sponsor;
 
 class ApiDoctorsController extends Controller
 {
@@ -15,7 +15,7 @@ class ApiDoctorsController extends Controller
 
     public function index()
     {
-        $users = User::all();
+        $users = User::with('profile')->paginate(10);
         return response()->json([
             'success' => true,
             'results' => $users
