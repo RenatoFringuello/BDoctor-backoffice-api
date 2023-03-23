90c9db05110c211e6fecc829400e382fbe140344
RenatoFringuello <renatofringuello@hotmail.it>
2023-03-23 10:08:20 +0100

    change header of the dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,22 +2,20 @@
 
 @section('content')
 <div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
     <div class="row justify-content-center">
-        <div class="col">
+        <div class="col pt-4">
             <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+                {{-- <div class="card-header">{{ __('User Dashboard') }}</div> --}}
 
                 <div class="card-body">
+                    <h2>Dashboard</h2>
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Welcome back '. Auth::user()->name) }}
                 </div>
             </div>
         </div>
