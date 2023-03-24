fdfa1cf9ecf7435c1b750e60eb3d24bf272a2e05
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-03-24 16:08:38 +0100

    add other 20 users on database

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -61,9 +61,9 @@ class UserTableSeeder extends Seeder
                 "isActive" => false
             ],
             [
-                "name" => "Rebecca",
-                "lastname" => "De Cillis",
-                "email" => "rebeccad@mailme.de",
+                "name" => "Sonia",
+                "lastname" => "De Marco",
+                "email" => "soniademarquez@mailme.de",
                 "password" => "123456789",
                 "isActive" => true
             ],
@@ -77,7 +77,7 @@ class UserTableSeeder extends Seeder
             [
                 "name" => "Alessia",
                 "lastname" => "De Angelis",
-                "email" => "alessiad@gmail.te",
+                "email" => "alessimad@gmail.te",
                 "password" => "123456789",
                 "isActive" => true
             ],
@@ -90,8 +90,8 @@ class UserTableSeeder extends Seeder
             ],
             [
                 "name" => "Daniel",
-                "lastname" => "De Claudis",
-                "email" => "danield@gmail.re",
+                "lastname" => "De Cillis",
+                "email" => "danield",
                 "password" => "123456789",
                 "isActive" => true
             ],
@@ -156,6 +156,146 @@ class UserTableSeeder extends Seeder
                 "lastname" => "Lommi",
                 "email" => "fralommi@gmail.te",
                 "password" => "123456789",
+                "isActive" => true
+            ],
+            [
+                "name" => "Alex",
+                "lastname" => "Catalano",
+                "email" => "alecata@gmail.re",
+                "password" => "123456669",
+                "isActive" => true
+            ],
+            [
+                "name" => "Donato",
+                "lastname" => "Di Felice",
+                "email" => "donadife@gmail.bn",
+                "password" => "1235556789",
+                "isActive" => true
+            ],
+            [
+                "name" => "Federica",
+                "lastname" => "Bonolis",
+                "email" => "boolisf@mailme.de",
+                "password" => "1233356789",
+                "isActive" => true
+            ],
+            [
+                "name" => "Emanuele",
+                "lastname" => "Sau",
+                "email" => "emasau@writeme.ge",
+                "password" => "1223456789",
+                "isActive" => false
+            ],
+            [
+                "name" => "Guglielmo",
+                "lastname" => "Elisei",
+                "email" => "guglieli@qwerty.re",
+                "password" => "123456788",
+                "isActive" => false
+            ],
+            [
+                "name" => "Federico",
+                "lastname" => "Alpelli",
+                "email" => "fedealp@resident.net",
+                "password" => "113456789",
+                "isActive" => false
+            ],
+            [
+                "name" => "Oriana",
+                "lastname" => "De Angelis",
+                "email" => "oridea@mailme.de",
+                "password" => "123446789",
+                "isActive" => true
+            ],
+            [
+                "name" => "Claudia",
+                "lastname" => "Inchingoli",
+                "email" => "clainchi@gmail.bn",
+                "password" => "123356789",
+                "isActive" => true
+            ],
+            [
+                "name" => "Alessia",
+                "lastname" => "De Cubellis",
+                "email" => "alessiad@gmail.te",
+                "password" => "223456789",
+                "isActive" => true
+            ],
+            [
+                "name" => "Manuel",
+                "lastname" => "Carta",
+                "email" => "manucarta@gmail.nv",
+                "password" => "123456799",
+                "isActive" => false
+            ],
+            [
+                "name" => "Daniel",
+                "lastname" => "Curci",
+                "email" => "danielc@gmail.re",
+                "password" => "222256789",
+                "isActive" => true
+            ],
+            [
+                "name" => "Carlotta",
+                "lastname" => "Boni",
+                "email" => "karol@gmail.tim",
+                "password" => "123456999",
+                "isActive" => true
+            ],
+            [
+                "name" => "Sara",
+                "lastname" => "Ricci",
+                "email" => "sarar@gmail.te",
+                "password" => "123226789",
+                "isActive" => true
+            ],
+            [
+                "name" => "Ethan",
+                "lastname" => "La Tora",
+                "email" => "ethanlt@gmail.te",
+                "password" => "333456789",
+                "isActive" => true
+            ],
+            [
+                "name" => "Renato",
+                "lastname" => "Sanches",
+                "email" => "renatosan@gmail.te",
+                "password" => "123456666",
+                "isActive" => true
+            ],
+            [
+                "name" => "Alessio",
+                "lastname" => "Pirelli",
+                "email" => "alessiop@gmail.te",
+                "password" => "555556789",
+                "isActive" => false
+            ],
+            [
+                "name" => "Dante",
+                "lastname" => "Sorrentino",
+                "email" => "dantesorrentino@gmail.te",
+                "password" => "777776789",
+                "isActive" => true
+            ],
+            [
+                "name" => "Pasquale",
+                "lastname" => "Abbatini",
+                "email" => "Pasqualeabba@gmail.te",
+                "password" => "666985479",
+                "isActive" => true
+            ],
+            [
+                "name" => "Fernando",
+                "lastname" => "Bruni",
+                "email" => "bferna@gmail.te",
+                "password" => "152469873",
+                "isActive" => true
+            ],
+            [
+                "name" => "Francesca",
+                "lastname" => "Caruso",
+                "email" => "fraca@gmail.te",
+                "password" => "458796521",
                 "isActive" => true
             ],
         ];
