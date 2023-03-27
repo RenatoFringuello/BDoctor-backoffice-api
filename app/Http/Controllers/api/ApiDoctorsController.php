f053d519ac66d073ff111ec4448230112d2af91f
Shelbot77 <alessiodeangelis021@gmail.com>
2023-03-27 12:01:17 +0200

    Raw query to get ratings average commit

--- app/Http/Controllers/api/ApiDoctorsController.php
+++ app/Http/Controllers/api/ApiDoctorsController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Specialization;
+use Illuminate\Support\Facades\DB;
+
 // use App\Models\Profile;
 // use App\Models\Sponsor;
 
@@ -16,19 +18,27 @@ class ApiDoctorsController extends Controller
     public function index(Request $request)
     {
         //restituisce tutti i dottori
-        $user_query = User::with(['profile', 'profile.specializations', 'sponsors']);
+        //$user_query = User::with(['profile', 'profile.specializations', 'sponsors', 'reviews']);
+        $user_query = User::with(['reviews']);
         if ($request->specializations) {
 
             //filtra i risultati per specializzazione
-            $user_query->whereHas('profile.specializations', function ($query) use ($request) {
+            $user_query->select('users.*', DB::raw('round(avg(\'reviews.rating\'), 0)as average'))->whereHas('profile.specializations', function ($query) use ($request) {
                 $query->where('name', $request->specializations);
-            });
+            })->groupBy('users.id')->orderBy('average')->get();
         }
-        if ($request->sponsors) {
+        /* SELECT ROUND(AVG(`reviews`.`rating`),0) AS `media`, `users`.*
+        FROM `users`
+        JOIN `reviews` ON `reviews`.`user_id`=`users`.`id`
+        GROUP BY `users`.`id`
+        ORDER BY `media` DESC; */
+
+
+        /*  if ($request->sponsors) {
             $user_query->whereHas('sponsors', function ($query) use ($request) {
                 $query->where('type', $request->sponsors);
             });
-        }
+        } */
         $users = $user_query->paginate(10);
         return response()->json([
             'success' => true,
