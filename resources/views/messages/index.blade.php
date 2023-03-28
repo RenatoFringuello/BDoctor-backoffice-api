c63b4a7aa78d9f1fd28fc7acb6038a9c0ca9e548
Shelbot77 <alessiodeangelis021@gmail.com>
2023-03-28 15:50:22 +0200

    layout adjustements for messages.index

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -1,27 +1,9 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
+@section('content')
+<div class="container pt-4">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Usando Vite -->
-    @vite(['resources/js/app.js'])
-</head>
-
-<body>
-    <div class="container p-5">
-        <table class="table table-striped">
+    <div class="card rounded-4">
+        <table class="table">
             <thead class="table">
                 <tr>
                     
@@ -34,30 +16,32 @@
             </thead>
             <tbody>
                 @foreach ($messages as $message)
-                    <tr>
-                        <th scope="row">{{$message->id}}</th>
-                        <td>{{$message->name}}</td>
-                        <td>{{$message->lastname}}</td>
-                        <td>{{$message->email}}</td>
-                        <td>{{$message->content}}</td>
-                        <td>
-                            <form action="{{route('messages.destroy', $message->id)}}" method="post" class="d-inline-block form-deleter pt-3">
-                                @csrf
-                                @method('DELETE')
-
-                                <button type="submit" class="btn btn-danger"> Delete </button>
-                            </form>
-                        </td>
-
-
-
-                       
+                <tr>
+                    
+                    <td>{{$message->name}}</td>
+                    <td>{{$message->lastname}}</td>
+                    <td>{{$message->email}}</td>
+                    <td>{{$message->content}}</td>
+                    <td>
+                        <form action="{{route('messages.destroy', $message->id)}}" method="post" class="d-inline-block form-deleter pt-3">
+                            @csrf
+                            @method('DELETE')
+                            
+                            <button type="submit" class="btn btn-danger"> Delete </button>
+                        </form>
+                    </td>
+                    
+                    
+                    
+                    
                     </tr>
                 @endforeach
             </tbody>
         </table>
         
     </div>
-</body>
+</div>
+@endsection    
 
-</html>
+
+
