e55487585eb35ebc0bdae3df599dbcc1675d27d5
Shelbot77 <alessiodeangelis021@gmail.com>
2023-03-28 14:56:45 +0200

    setup routes for MessagesController

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::put('profile/update', [AuthProfileController::class, 'update'])
         ->name('profile.register.update');
-    Route::get('/messages', [MessagesController::class, 'index'])->name('messages.index');
+    //Route::get('/messages', [MessagesController::class, 'index'])->name('messages.index');
+    Route::resource('/messages', MessagesController::class);
 });
 
 require __DIR__ . '/auth.php';
