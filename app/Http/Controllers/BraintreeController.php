c7183f4a70d325eca7e2e6d52e39327558b57b6f
RenatoFringuello <renatofringuello@hotmail.it>
2023-03-30 13:16:45 +0200

    add sponsor to view in token method, add process method

--- app/Http/Controllers/BraintreeController.php
+++ app/Http/Controllers/BraintreeController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sponsor;
 use Illuminate\Http\Request;
 use Braintree\Gateway as Braintree_Gateway;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class BraintreeController extends Controller
 {
     public function token(Request $request)
     {
-
         $gateway = new Braintree_Gateway([
             'environment' => 'sandbox',
             'merchantId' => '3xb8y75mrh9cyy98',
@@ -17,6 +19,14 @@ class BraintreeController extends Controller
             'privateKey' => '6743c0d02106c81481dac2a90458c7d7'
         ]);
         $clientToken = $gateway->clientToken()->generate();
-        return view('braintree', ['token' => $clientToken]);
+        $sponsor = Sponsor::findOrFail($request->sponsor);
+        return view('braintree', ['token' => $clientToken, 'sponsor' => $sponsor]);
+    }
+
+    public function process(Request $request) {
+        //pagamento avvenuto con successo
+        Auth::user()->sponsors()->sync([$request->sponsor]);
+        // dd(Auth::user()->sponsors);
+        return Redirect::route('dashboard', ['paymentStatus'=> $request->paymentStatus]);
     }
 }
