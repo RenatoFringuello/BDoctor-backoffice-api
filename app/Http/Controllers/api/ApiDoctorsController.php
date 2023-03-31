e7eec927e3115f55c47325692714bd846fabfe5d
RenatoFringuello <renatofringuello@hotmail.it>
2023-03-31 16:02:57 +0200

    add api index with merge collections without paginate

--- app/Http/Controllers/api/ApiDoctorsController.php
+++ app/Http/Controllers/api/ApiDoctorsController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Specialization;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
 class ApiDoctorsController extends Controller
 {
@@ -13,30 +15,59 @@ class ApiDoctorsController extends Controller
     {
         if ($request->specializations && Specialization::where('name', '=', $request->specializations)->get()->toArray()) {
             //filtra i risultati per specializzazione(sempre) AND se la specializzazione richiesta esiste; 
-            $user_query = User::with(['profile', 'profile.specializations', 'sponsors', 'reviews'])
+            // $users_query = User::with(['profile', 'profile.specializations', 'sponsors', 'reviews'])
+            //     ->withAvg('reviews', 'rating')
+            //     ->withCount('reviews')
+            //     ->join('sponsor_user', 'sponsor_user.user_id', '=', 'users.id')
+            //     ->join('sponsors', 'sponsors.id', '=', 'sponsor_user.sponsor_id')
+            //     ->whereHas('profile.specializations', function ($query) use ($request) {
+            //         $query->where('name', $request->specializations);
+            //     })->orderBy('sponsors.id', 'DESC');
+
+            $users_query_featured = User::with(['profile', 'profile.specializations', 'sponsors', 'reviews'])
                 ->withAvg('reviews', 'rating')
                 ->withCount('reviews')
                 ->whereHas('profile.specializations', function ($query) use ($request) {
                     $query->where('name', $request->specializations);
+                })
+                ->whereHas('sponsors', function ($query) use ($request) {
+                    $query->where('id', '<>', '1');
+                });
+            $users_query_not_featured = User::with(['profile', 'profile.specializations', 'sponsors', 'reviews'])
+                ->withAvg('reviews', 'rating')
+                ->withCount('reviews')
+                ->whereHas('profile.specializations', function ($query) use ($request) {
+                    $query->where('name', $request->specializations);
+                })
+                ->whereHas('sponsors', function ($query) use ($request) {
+                    $query->where('id', '=', '1');
                 });
 
             if ($request->sortByAvg) {
                 //se viene richiesto un sort per avg
-                $user_query->orderBy('reviews_avg_rating', 'DESC');
+                // $users_query->orderBy('reviews_avg_rating', 'DESC');
+
+                $users_query_featured->orderBy('reviews_avg_rating', 'DESC');
+                $users_query_not_featured->orderBy('reviews_avg_rating', 'DESC');
             }
             if ($request->sortByCount) {
                 //se viene richiesto un sort per count
-                $user_query->orderBy('reviews_count', 'DESC');
+                // $users_query->orderBy('reviews_count', 'DESC');
+
+                $users_query_featured->orderBy('reviews_count', 'DESC');
+                $users_query_not_featured->orderBy('reviews_count', 'DESC');
             }
 
-            //get the results
-            $user_query->get();
             //then create the pagination
-            $user = $user_query->paginate(10);
+            // $users_query->get();
+            // $users = $users_query->paginate(10);
+
+            $users = $users_query_featured->get()->merge($users_query_not_featured->get());
+            // $users = (new Collection($users_query_featured->get()->merge($users_query_not_featured->get())))->paginate(10);
 
             return response()->json([
                 'success' => true,
-                'results' => $user
+                'results' => $users,
             ]);
         } else {
             //se la ricerca non viene fatta per specializzazione allora return false
@@ -62,7 +93,7 @@ class ApiDoctorsController extends Controller
 
     public function sponsored(Request $request)
     {
-        $user_query = User::with(['profile', 'profile.specializations', 'sponsors', 'reviews'])
+        $users_query = User::with(['profile', 'profile.specializations', 'sponsors', 'reviews'])
             ->withAvg('reviews', 'rating')
             ->withCount('reviews')
             ->whereHas('sponsors', function ($query) use ($request) {
@@ -74,7 +105,7 @@ class ApiDoctorsController extends Controller
         //se la ricerca non viene fatta per specializzazione allora return false
         return response()->json([
             'success' => true,
-            'results' => $user_query
+            'results' => $users_query
         ]);
     }
 }
