606f13e85240d787c11f111afb0ec9b4e6d18154
RenatoFringuello <renatofringuello@hotmail.it>
2023-03-31 16:51:01 +0200

    add paginate function to paginate collection

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * generate a LengthAwarePaginator paginaton with a collection passed
+     *
+     * @param Collection $collection
+     * @param integer $perPage
+     * @param int $page
+     * @param array $options
+     * @return LengthAwarePaginator collection paginated
+     */
+    public function paginate($collection, $perPage = 15, $page = null, $options = [])
+    {
+        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+        $collection = $collection instanceof Collection ? $collection : Collection::make($collection);
+        return new LengthAwarePaginator($collection->forPage($page, $perPage), $collection->count(), $perPage, $page, $options);
+    }
 }
