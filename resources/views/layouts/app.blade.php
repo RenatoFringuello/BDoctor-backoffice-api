e45e89ec24f42c21b8d10f4713b83b62306fd33b
RenatoFringuello <renatofringuello@hotmail.it>
2023-04-01 18:49:44 +0200

    add title and icon in header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'BDoctor') }}</title>
+
+    <link rel="icon" type="image/svg+xml" href="{{asset('assets/B-Doc-Logo.png')}}" />
     
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -21,9 +23,6 @@
     @vite(['resources/js/app.js'])
     @vite(['resources/js/client-side-validations.js'])
     
-
-
-
 </head>
 
 <body>
